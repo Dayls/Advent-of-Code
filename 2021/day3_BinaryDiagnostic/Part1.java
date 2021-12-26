3292fea96d948900963d0bc84fc3a48ae01f6071
danil <daniil.shmel@list.ru>
2021-12-26 20:01:46 +0400

    Refactor Day3_Part1 solution

--- 2021/day3_BinaryDiagnostic/Part1.java
+++ 2021/day3_BinaryDiagnostic/Part1.java
@@ -4,14 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Part1 {
+	private static final List<Character> gammaRate = new ArrayList<>();
+	private static final List<Character> epsilonRate = new ArrayList<>();
+
 	public static void main(String[] args) {
+		long powerConsumption = getPowerConsumption("2021/day3_BinaryDiagnostic/input.txt");
+		System.out.println(powerConsumption);
+
+	}
+
+	private static long getPowerConsumption(String filePath) {
+		makeGammaAndEpsilonRate(filePath);
+		long epsilonRateBinary = getEpsilonRateInBinary();
+		long gammaRateBinary = getGammaRateInBinary();
+
+		long epsilonRateDecimal = BinaryConverter.convertToDecimal(epsilonRateBinary);
+		long gammaRateDecimal = BinaryConverter.convertToDecimal(gammaRateBinary);
+		return epsilonRateDecimal * gammaRateDecimal;
+	}
+
+	private static void makeGammaAndEpsilonRate(String path) {
+		List<List<Character>> data = getLines(path);
+		calculateGammaAndEpsilonRate(data);
+	}
+
+	private static List<List<Character>> getLines(String path) {
 		Loader loader = new Loader("2021/day3_BinaryDiagnostic/input.txt");
-		List<List<Character>> data = loader.getLines();
+		return loader.getLines();
+	}
 
-		List<Character> gammaRate = new ArrayList<>();
-		List<Character> epsilonRate = new ArrayList<>();
-
-		int numberCount = 0;
+	private static void calculateGammaAndEpsilonRate(List<List<Character>> data) {
 		int index = 0;
 		int current0 = 0, current1 = 0;
 		for (int i = 0; i < data.size(); i++) {
@@ -24,7 +46,6 @@ public class Part1 {
 
 			if (i == data.size() - 1) {
 				if (current1 > current0) {
-
 					gammaRate.add('1');
 					epsilonRate.add('0');
 				} else {
@@ -41,16 +62,28 @@ public class Part1 {
 				current1 = 0;
 				i = -1;
 			}
-
-		}
-		System.out.println();
-		for (char c : gammaRate) {
-			System.out.print(c);
-		}
-		System.out.println();
-		for (char c : epsilonRate) {
-			System.out.print(c);
 		}
 	}
-}
 
+	private static long getGammaRateInBinary() {
+		long gammaRateBinary = 0;
+		StringBuilder builder = new StringBuilder();
+
+		for (char c : gammaRate) {
+			builder.append(c);
+		}
+		gammaRateBinary = Long.parseLong(builder.toString());
+		return gammaRateBinary;
+	}
+
+	private static long getEpsilonRateInBinary() {
+		long epsilonRateBinary = 0;
+		StringBuilder builder = new StringBuilder();
+
+		for(char c : epsilonRate) {
+			builder.append(c);
+		}
+		epsilonRateBinary = Long.parseLong(builder.toString());
+		return epsilonRateBinary;
+	}
+}
